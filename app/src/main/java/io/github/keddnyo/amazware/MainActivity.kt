61417aa86a8eb33a7c9ea4f661509f7726aefc8c
Keddnyo <SuperGen300@gmail.com>
2022-02-22 00:56:41 +0300

    Initial commit

---
+++ app/src/main/java/io/github/keddnyo/amazware/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.github.keddnyo.amazware
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
